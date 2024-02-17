d86d09afb3e7a16b8a6f8902530aa16e8446794b
Saulo Christian <saullochristian@outlook.com>
2024-02-17 10:52:34 -0300

    Criação do elements.js

---
+++ js/elements.js
@@ -0,0 +1,5 @@
+const bgHome = document.body.querySelector("#linkHome")
+const bgUniverse = document.body.querySelector("#linkUniverso")
+const bgExploration = document.body.querySelector("#linkExploracao")
+
+export { bgHome, bgUniverse, bgExploration};
