467aac2aa7abe4f5e983ad9362271f8d6c09acc8
Saulo Christian <saullochristian@outlook.com>
2024-02-17 13:57:12 -0300

    Criada a rota das páginas, remoção dos menus dos arquivos HTML do diretório pages.

--- js/event.js
+++ js/event.js
@@ -1,20 +1,20 @@
 export function Backgrounds({bgHome, bgUniverse, bgExploration}){
     function backgroundHome() {
-        document.body.classList.remove('universo')
-        document.body.classList.remove('exploracao')
+        document.body.classList.remove('universe')
+        document.body.classList.remove('exploration')
         document.body.classList.add('home')
     }
 
     function backgroundUniverso() {
         document.body.classList.remove('home')
-        document.body.classList.remove('exploracao')
-        document.body.classList.add('universo')
+        document.body.classList.remove('exploration')
+        document.body.classList.add('universe')
     }
 
     function backgroundExploracao() {
-        document.body.classList.remove('universo')
+        document.body.classList.remove('universe')
         document.body.classList.remove('home')
-        document.body.classList.add('exploracao')
+        document.body.classList.add('exploration')
     }
 
     return {
